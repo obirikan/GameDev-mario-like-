3a0d8ad63f264b405ecf054ed77adb671d3744c5
unknown <obirikan020@gmail.com>
2022-07-28 00:01:51 +0000

    allows player to shoot circles  to desired point

--- logic.js
+++ logic.js
@@ -1,7 +1,9 @@
+import move from './imgs/player'
 const canvas=document.getElementById('context')
 const c=canvas.getContext('2d')
 canvas.width=innerWidth
 canvas.height=innerHeight
+console.log(move)
 
 const gravity=1.5
 
@@ -18,10 +20,13 @@ class Player{
         }
         this.width=30
         this.height=30
+        // this.image=createImage(p)
     }
     draw(){
         c.fillStyle='red'
         c.fillRect(this.position.x,this.position.y,this.width,this.height)
+        c.fill()
+        // c.drawImage(this.image,this.position.x,this.position.y)
     }
     update(){ 
         this.draw()    
@@ -50,49 +55,66 @@ class Shoot{
         // this.x=a
         // this.y=b
        
-        this.radius=10
+        this.radius=5
     }
     draw(){
         c.beginPath()
-        c.arc(this.x,this.y,this.radius,0,Math.PI*2,false)
+        c.arc(this.x+20,this.y-5,this.radius,0,Math.PI*2,false)
+        c.fillStyle='blue'
         c.fill()
         // c.fillRect(this.x,this.y,this.width,this.height)
     }
     update(){ 
         this.draw()
-        this.x += this.velocity.x +2
-        this.y += this.velocity.y  +2    
+        this.x+= this.velocity.x+this.velocity.x+this.velocity.x+this.velocity.x
+        this.y += this.velocity.y+this.velocity.y+this.velocity.y+this.velocity.y+this.velocity.y
+
+    }
+    ps(){ 
+        this.draw()
+        this.x+= this.velocity.x+this.velocity.x+this.velocity.x+this.velocity.x+this.velocity.x+this.velocity.x
+        this.y += this.velocity.y+this.velocity.y+this.velocity.y+this.velocity.y+this.velocity.y+this.velocity.y+this.velocity.y
+
     }
 }
 const sho=new Shoot
 console.log(sho)
 const bullets=[]
-
-
-// addEventListener('click',()=>{
-//     bullets.push(sho)
-// })
+const power=[]
 
 
 addEventListener('click',({clientX,clientY})=>{
     const angle=Math.atan2(clientY-player.position.y,clientX-player.position.x)
-    console.log(angle)
+ 
     const velocity1={
         x:Math.cos(angle),
         y:Math.sin(angle)
     }
     const neww=new Shoot(velocity1)
+    const new1=new Shoot(velocity1)
     bullets.push(neww)
+    power.push(new1)
 })
 
 function animate(){
     requestAnimationFrame(animate)
     c.clearRect(0,0,canvas.width,canvas.height)
-  
-    bullets.forEach(s=>{
-        s.update()
-    })
     player.update() 
+    // bullets.forEach(s=>{
+    //     s.update()
+        
+    // })
+    power.forEach(s=>{
+        s.ps()  
+        s.ps()  
+        s.ps()  
+        s.ps()
+        s.ps()
+        s.ps()
+        s.ps()
+        s.ps()
+    })
+
 }
 animate()
 
