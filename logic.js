a0ddb681e7849dda40a2265f964ec86de473d582
obirikan <obirikan020@gmail.com>
2023-06-02 15:01:23 +0100

    fixed bug

--- logic.js
+++ logic.js
@@ -1,38 +1,39 @@
+// import move from './imgs/player'
 const canvas=document.getElementById('context')
 const c=canvas.getContext('2d')
 canvas.width=innerWidth
 canvas.height=innerHeight
+// console.log(move)
 
 const gravity=1.5
-let a=0
-let b=0
+
 class Player{
     constructor(){
+        //the postion of the player on the plane[x,y]
         this.position={
-            x:100,
-            y:100
+            x:200,
+            y:200
         }
-
-        this.newpo={
-            a,
-            b
-        }
-
+        //the speed of the player
         this.velocity={
             x:0,
             y:0
         }
-        this.width=30
-        this.height=30
+        //the size of the player
+        this.width=60
+        this.height=60
+        // this.image=createImage(p)
     }
+    //this draws out the player
     draw(){
         c.fillStyle='red'
         c.fillRect(this.position.x,this.position.y,this.width,this.height)
+        c.fill()
     }
     update(){ 
         this.draw()    
-        this.newpo.b= this.position.y += this.velocity.y
-        this.newpo.a= this.position.x += this.velocity.x
+         this.position.y += this.velocity.y
+         this.position.x += this.velocity.x
       
         if(this.velocity.y + this.height +this.position.y<=canvas.height){
             this.velocity.y =0
@@ -47,61 +48,65 @@ player.update()
 
 
 class Shoot{
-    constructor(a,b){
-        this.velocity={
-            x:0,
-            y:1
-        }
+    constructor(velocity1){
+        this.velocity=velocity1
+
         this.x=player.position.x
         this.y=player.position.y
 
         // this.x=a
         // this.y=b
        
-        this.radius=30
+        this.radius=5
     }
     draw(){
         c.beginPath()
-        c.arc(this.x,this.y,this.radius,0,Math.PI*2,false)
+        c.arc(this.x+20,this.y-5,this.radius,0,Math.PI*2,false)
+        c.fillStyle='blue'
         c.fill()
         // c.fillRect(this.x,this.y,this.width,this.height)
     }
     update(){ 
-        this.draw()    
-         this.y += this.velocity.y
-        // this.newpo.a= this.position.x += this.velocity.x
-      
-        if(this.velocity.y +this.radius<=canvas.height){
-            this.velocity.y =gravity
-        }else{
-            this.velocity.y=0
-        }
+        this.draw()
+        this.x+= this.velocity.x+this.velocity.x+this.velocity.x+this.velocity.x
+        this.y += this.velocity.y+this.velocity.y+this.velocity.y+this.velocity.y+this.velocity.y
+
+    }
+    ps(){ 
+        this.draw()
+        this.x+= this.velocity.x+this.velocity.x+this.velocity.x+this.velocity.x+this.velocity.x+this.velocity.x
+        this.y += this.velocity.y+this.velocity.y+this.velocity.y+this.velocity.y+this.velocity.y+this.velocity.y+this.velocity.y
+
     }
 }
 const sho=new Shoot
 console.log(sho)
 const bullets=[]
+const power=[]
 
 
-// addEventListener('click',()=>{
-//     bullets.push(sho)
-// })
-
-
-addEventListener('click',()=>{
-    console.log(player.newpo.a)
-    const neww=new Shoot(player.newpo.a,player.newpo.b)
+addEventListener('click',({clientX,clientY})=>{
+    const angle=Math.atan2(clientY-player.position.y,clientX-player.position.x)
+ 
+    const velocity1={
+        x:Math.cos(angle),
+        y:Math.sin(angle)
+    }
+    const neww=new Shoot(velocity1)
+    const new1=new Shoot(velocity1)
     bullets.push(neww)
+    power.push(new1)
 })
 
 function animate(){
     requestAnimationFrame(animate)
     c.clearRect(0,0,canvas.width,canvas.height)
-  
-    bullets.forEach(s=>{
-        s.update()
-    })
     player.update() 
+
+    power.forEach(s=>{
+        s.ps()  
+    })
+
 }
 animate()
 
